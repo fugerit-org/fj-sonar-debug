ddb584e76cd4c1847eb21dd7fd92ef93d16ba3bc
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-08-25 14:55:46 +0200

    2 system out

--- src/main/java/org/fugerit/java/sonar/debug/SystemOut.java
+++ src/main/java/org/fugerit/java/sonar/debug/SystemOut.java
@@ -3,7 +3,8 @@ package org.fugerit.java.sonar.debug;
 public class SystemOut {
 	
 	public void myMethod() {
-		System.out.println( "TEST" );
+		System.out.println( "TEST 1" );
+		System.out.println( "TEST 2" );
 	}
 	
 }
