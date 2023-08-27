158eff65d9bf78fa7d5562f2a6ef67e61e006b6a
Matteo Franci a.k.a. Fugerit <m@fugerit.org>
2023-08-27 23:05:56 +0200

    Converage test

--- src/test/java/test/org/fugerit/java/sonar/debug/TestBooleanExpressionsShouldNotBeGratuitousS2589.java
+++ src/test/java/test/org/fugerit/java/sonar/debug/TestBooleanExpressionsShouldNotBeGratuitousS2589.java
@@ -9,6 +9,17 @@ public class TestBooleanExpressionsShouldNotBeGratuitousS2589 {
 	@Test
 	public void testBase() {
 		boolean ok = false;
+		System.setProperty( "my-prop" , "false" );
+		BooleanExpressionsShouldNotBeGratuitousS2589 inst = new BooleanExpressionsShouldNotBeGratuitousS2589();
+		inst.myMethod();
+		ok = true;
+		Assert.assertTrue( ok );
+	}
+
+	@Test
+	public void testBaseProp() {
+		boolean ok = false;
+		System.setProperty( "my-prop" , "true" );
 		BooleanExpressionsShouldNotBeGratuitousS2589 inst = new BooleanExpressionsShouldNotBeGratuitousS2589();
 		inst.myMethod();
 		ok = true;
